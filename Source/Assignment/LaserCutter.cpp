50e042090e8a3023d9bf3357dd7fad487b8f59d5
William Tang <galaxyking0419@gmail.com>
2020-09-24 14:00:02 +0800

    Add LaserCutter Class

---
+++ Source/Assignment/LaserCutter.cpp
@@ -0,0 +1,49 @@
+#include "LaserCutter.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+#include "PlayerCharacter.h"
+
+ALaserCutter::ALaserCutter()
+	: TimeElapsed(0.0f)
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	/* Laser Mesh */
+	LaserMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Laser Root Mesh Component"));
+	RootComponent = LaserMeshComp;
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> LaserMeshAsset(TEXT("StaticMesh'/Engine/BasicShapes/Cylinder.Cylinder'"));
+	LaserMeshComp->SetStaticMesh(LaserMeshAsset.Object);
+
+	ConstructorHelpers::FObjectFinder<UMaterial> LaserMaterialAsset(TEXT("Material'/Game/Materials/M_Laser.M_Laser'"));
+	LaserMeshComp->SetMaterial(0, LaserMaterialAsset.Object);
+
+	LaserMeshComp->SetRelativeScale3D(FVector(0.125f, 0.125f, 4.0f));
+	LaserMeshComp->SetCollisionProfileName("Trigger");
+	LaserMeshComp->OnComponentBeginOverlap.AddDynamic(this, &ALaserCutter::OnLaserBeginOverlap);
+}
+
+void ALaserCutter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	TimeElapsed += DeltaTime;
+	LerpCurrent = UKismetMathLibrary::VInterpTo_Constant(LerpStart, LerpEnd, TimeElapsed, 128.0f);
+
+	if ((LerpEnd - LerpCurrent).IsNearlyZero(0.5f)) {
+		SetActorLocation(LerpEnd);
+		LerpEnd = LerpStart;
+		LerpStart = GetActorLocation();
+		TimeElapsed = 0.0f;
+	} else {
+		SetActorLocation(LerpCurrent);
+	}
+}
+
+void ALaserCutter::OnLaserBeginOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor,
+	UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
+{
+	if (Cast<APlayerCharacter>(OtherActor))
+		OtherActor->TakeDamage(-0.2f, FDamageEvent(), nullptr, this);
+}
---
+++ Source/Assignment/LaserCutter.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/Actor.h"
+
+#include "LaserCutter.generated.h"
+
+UCLASS()
+class ASSIGNMENT_API ALaserCutter : public AActor
+{
+	GENERATED_BODY()
+
+private:
+	UStaticMeshComponent *LaserMeshComp;
+
+	UPROPERTY(EditAnywhere)
+	FVector LerpStart;
+
+	UPROPERTY(EditAnywhere)
+	FVector LerpEnd;
+
+	float TimeElapsed;
+	FVector LerpCurrent;
+
+public:
+	ALaserCutter();
+	void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnLaserBeginOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor,
+		UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+};
